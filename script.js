91492a576db5c8562c03b6a51a00c0a45ef77f1e
shoaibkhan73 <khan973973@gmail.com>
2024-11-27 20:31:49 +0500

    finish practic no 1

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 alert("hellow world");
+
+console.log("hi i am consol")
+
+var a = prompt("enter your code")
+
+console.log("your code is" + a)
+
+var isturue = confirm("Are you sure to agre with this code")
+
+if(isturue){
+    console.log("yor are agre to conform")
+}
+else {
+    console.log("yor are not agre to conform")
+}
